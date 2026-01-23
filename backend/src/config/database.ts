946ca3a95feac581695d8fe9fa372ca0753154f1
markw53 <m.workman@outlook.com>
2026-01-23 10:04:26 +0000

    added console logs

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,6 +4,8 @@ export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI!);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`Database Name: ${conn.connection.name}`);  
+    console.log(`Connection String: ${process.env.MONGODB_URI}`);
   } catch (error: any) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
